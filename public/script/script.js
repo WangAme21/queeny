bae0dcc30b9a46a1b5936a9b934aa0430a4bbb85
WangAme21 <rizadajohn5@gmail.com>
2025-10-14 17:18:07 +0800

    updated

--- public/script/script.js
+++ public/script/script.js
@@ -1,6 +1,27 @@
 const hamburger = document.getElementById('hamburger');
 const navLinks = document.getElementById('nav-links');
+const cakeBtn = document.getElementById('cake-btn');
+const dishBtn = document.getElementById('dish-btn');
+const otherBtn = document.getElementById('other-btn');
 
 hamburger.addEventListener("click", ()=>{
     navLinks.classList.toggle("active");
 })
+
+function openDishCategory(){
+    document.getElementById('cake-category').style.display = 'none';
+    document.getElementById('other-category').style.display = 'none';
+    document.getElementById('dish-category').style.display = 'block';
+}
+
+function openCakeCategory(){
+    document.getElementById('cake-category').style.display = 'block';
+    document.getElementById('other-category').style.display = 'none';
+    document.getElementById('dish-category').style.display = 'none';
+}
+
+function openOtherCategory(){
+    document.getElementById('cake-category').style.display = 'none';
+    document.getElementById('other-category').style.display = 'block';
+    document.getElementById('dish-category').style.display = 'none';
+}
